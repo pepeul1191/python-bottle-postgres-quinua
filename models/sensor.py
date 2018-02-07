c71ad5c8021cf9ce1b1cd23ffe5e9d52f96bc207
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-07 10:49:22 -0500

    Reportes en python decimal a float

--- models/sensor.py
+++ models/sensor.py
@@ -16,3 +16,35 @@ class Sensor(DB):
       tmp = {'dia': r['dia'], 'maximo': float(r['maximo']), 'minimo': float(r['minimo']), 'promedio': float(r['promedio']), 'mediciones': r['mediciones']}
       rpta.append(tmp)
     return rpta
+
+  def promedio_rango_fechas(self, sensor_id, fecha_inicio, fecha_fin):
+    self.cursor.execute("""
+      SELECT to_char(fec_med, 'YYYY') || '/' || to_char(fec_med, 'MM') || '/' || to_char(fec_med, 'DD') AS dia, 
+      AVG(valor_med) AS promedio, COUNT(valor_med) AS mediciones 
+      FROM inve_instru_dato WHERE fec_med > %s AND fec_med < %s AND ide_sensor = %s
+      GROUP BY dia;
+    """, (fecha_inicio, fecha_fin, sensor_id))
+    rs = self.cursor.fetchall()
+    self.cursor.close()
+    self.connection.close()
+    rpta = []
+    for r in rs:
+      tmp = {'dia': r['dia'], 'promedio': float(r['promedio']), 'mediciones': r['mediciones']}
+      rpta.append(tmp)
+    return rpta
+
+  def maximo_minimo_rango_fechas(self, sensor_id, fecha_inicio, fecha_fin):
+    self.cursor.execute("""
+      SELECT to_char(fec_med, 'YYYY') || '/' || to_char(fec_med, 'MM') || '/' || to_char(fec_med, 'DD') AS dia, 
+      MAX(valor_med) AS maximo, MIN(valor_med) AS minimo, COUNT(valor_med) AS mediciones 
+      FROM inve_instru_dato WHERE fec_med > %s AND fec_med < %s AND ide_sensor = %s
+      GROUP BY dia;
+    """, (fecha_inicio, fecha_fin, sensor_id))
+    rs = self.cursor.fetchall()
+    self.cursor.close()
+    self.connection.close()
+    rpta = []
+    for r in rs:
+      tmp = {'dia': r['dia'], 'maximo': float(r['maximo']), 'minimo': float(r['minimo']), 'mediciones': r['mediciones']}
+      rpta.append(tmp)
+    return rpta
--- views/sensor.py
+++ views/sensor.py
@@ -15,3 +15,21 @@ def maximo_minimo_promedio_rango_fechas():
   fecha_inicio = request.query.fecha_inicio
   fecha_fin = request.query.fecha_fin
   return json.dumps(Sensor().maximo_minimo_promedio_rango_fechas(sensor_id, fecha_inicio, fecha_fin))
+
+#http://192.168.1.54:3025/sensor/promedio_rango_fechas?sensor_id=1&fecha_inicio=2017-08-18&fecha_fin=2017-08-21
+@sensor_view.route('/promedio_rango_fechas', method='GET')
+@enable_cors
+def promedio_rango_fechas():
+  sensor_id = request.query.sensor_id
+  fecha_inicio = request.query.fecha_inicio
+  fecha_fin = request.query.fecha_fin
+  return json.dumps(Sensor().promedio_rango_fechas(sensor_id, fecha_inicio, fecha_fin))
+
+#http://192.168.1.54:3025/sensor/maximo_minimo_rango_fechas?sensor_id=1&fecha_inicio=2017-08-18&fecha_fin=2017-08-21
+@sensor_view.route('/maximo_minimo_rango_fechas', method='GET')
+@enable_cors
+def maximo_minimo_rango_fechas():
+  sensor_id = request.query.sensor_id
+  fecha_inicio = request.query.fecha_inicio
+  fecha_fin = request.query.fecha_fin
+  return json.dumps(Sensor().maximo_minimo_rango_fechas(sensor_id, fecha_inicio, fecha_fin))
