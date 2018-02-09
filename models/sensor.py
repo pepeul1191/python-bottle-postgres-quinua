384f803d1b17aa0e35305d4ca1f36117249781be
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-09 11:41:41 -0500

    Mediciones por dia en rango de momento - python

--- models/sensor.py
+++ models/sensor.py
@@ -48,3 +48,20 @@ class Sensor(DB):
       tmp = {'dia': r['dia'], 'maximo': float(r['maximo']), 'minimo': float(r['minimo']), 'mediciones': r['mediciones']}
       rpta.append(tmp)
     return rpta
+
+  def rango_tiempo_dia(self, sensor_id, dia_medicion, hora_inicio, hora_fin):
+    self.cursor.execute("""
+      SELECT to_char(fec_med, 'HH') || ':' || to_char(fec_med, 'MI') || ':' || to_char(fec_med, 'ss') AS momento, 
+      valor_med AS dato 
+      FROM inve_instru_dato WHERE fec_med > %s AND fec_med < %s AND ide_sensor = %s 
+    """, (dia_medicion + ' ' + hora_inicio, dia_medicion + ' ' + hora_fin, sensor_id))
+    rs = self.cursor.fetchall()
+    self.cursor.close()
+    self.connection.close()
+    rpta = []
+    for r in rs:
+      tmp = {'momento': r['momento'], 'dato': float(r['dato'])}
+      rpta.append(tmp)
+    return rpta
+
+    
--- views/sensor.py
+++ views/sensor.py
@@ -33,3 +33,13 @@ def maximo_minimo_rango_fechas():
   fecha_inicio = request.query.fecha_inicio
   fecha_fin = request.query.fecha_fin
   return json.dumps(Sensor().maximo_minimo_rango_fechas(sensor_id, fecha_inicio, fecha_fin))
+
+#http://192.168.1.54:3025/sensor/rango_tiempo_dia?dia_medicion=2017-08-18&sensor_id=3&hora_inicio=00:00&hora_fin=00:14 
+@sensor_view.route('/rango_tiempo_dia', method='GET')
+@enable_cors
+def rango_tiempo_dia():
+  sensor_id = request.query.sensor_id
+  hora_inicio = request.query.hora_inicio
+  hora_fin = request.query.hora_fin
+  dia_medicion = request.query.dia_medicion
+  return json.dumps(Sensor().rango_tiempo_dia(sensor_id, dia_medicion, hora_inicio, hora_fin))
